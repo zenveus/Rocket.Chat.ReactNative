26a419e743614605094f55a804dbe9f8c315e425
kumailp <kumailpirzada@gmail.com>
2022-03-15 17:17:27 +0500

    add: tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,9 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
